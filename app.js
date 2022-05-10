75b8956de72e17a14340c36081b0ce87210ec3ff
Tyler Rice <Tylerrayrice2@gmail.com>
2022-05-09 22:47:39 -0600

    Moody-Kitten Final

--- app.js
+++ app.js
@@ -7,13 +7,44 @@ let kittens = []
  * Then reset the form
  */
 function addKitten(event) {
+  event.preventDefault()
+  let form = event.target
+
+  let kitten = {
+    id: generateId(),
+    name: form.name.value,
+    mood: "Tolerant",
+    affection: 5,
+  }
+
+  // This code finds kittens with the same name and does not allow a
+  //duplicate to be submitted
+  let Duplicate = kittens.find((kitten) => kitten.name === form.name.value);
+  if (Duplicate) {
+    console.log("Kitten already exists!");
+    { alert("Kitten already exists!") }
+
+    return;
+  }
+
+
+  console.log(kitten);
+
+  kittens.push(kitten)
+  saveKittens()
+  form.reset()
+  drawKittens()
 }
+
 
 /**
  * Converts the kittens array to a JSON string then
  * Saves the string to localstorage at the key kittens 
  */
 function saveKittens() {
+  window.localStorage.setItem("kittens", JSON.stringify(kittens))
+  drawKittens()
+
 }
 
 /**
@@ -22,12 +53,35 @@ function saveKittens() {
  * the kittens array to the retrieved array
  */
 function loadKittens() {
+  let storedKittens = JSON.parse(window.localStorage.getItem
+    ("kittens"))
+  if (storedKittens) {
+    kittens = storedKittens
+  }
 }
 
 /**
  * Draw all of the kittens to the kittens element
  */
+
 function drawKittens() {
+  loadKittens()
+  let kittenListElement = document.getElementById("kittens")
+  let kittensTemplate = ""
+  kittens.forEach(kitten => {
+    kittensTemplate += `
+    <div class="cat-border bg-dark kitten ${kitten.mood} text-light">
+      <img class="kitten" src="small-cat.png"${kitten.name}size=200x200">
+      <div class="d-flex justify-content-center">Name: ${kitten.name}</div>
+      <div class="d-flex justify-content-center">Mood: ${kitten.mood}</div>
+      <div class="d-flex justify-content-center">Affection: ${kitten.affection}</div>
+      <div class="d-flex space-between"></div>
+      <button class="btn-cancel m-3" onclick="pet('${kitten.id}')">Pet</button>
+      <button class="m-1" onclick="catnip('${kitten.id}')">Catnip</button>
+    </div>
+    `
+  })
+  kittenListElement.innerHTML = kittensTemplate
 }
 
 
@@ -36,7 +90,10 @@ function drawKittens() {
  * @param {string} id 
  * @return {Kitten}
  */
+
+
 function findKittenById(id) {
+  return kittens.find(kitten => kitten.id == id)
 }
 
 
@@ -49,6 +106,17 @@ function findKittenById(id) {
  * @param {string} id 
  */
 function pet(id) {
+  let Kitten = findKittenById(id)
+  let randomNumber = Math.random()
+  if (randomNumber > .5) {
+    Kitten.affection++;
+    setKittenMood(Kitten)
+    saveKittens()
+
+  }
+  else Kitten.affection--;
+  setKittenMood(Kitten)
+  saveKittens()
 }
 
 /**
@@ -58,29 +126,47 @@ function pet(id) {
  * @param {string} id
  */
 function catnip(id) {
+  let Kitten = findKittenById(id)
+  Kitten.mood = "Tolerant"
+  Kitten.affection = 5
+  saveKittens()
+
 }
 
 /**
  * Sets the kittens mood based on its affection
  * @param {Kitten} kitten 
  */
-function setKittenMood(kitten) {
+function setKittenMood(Kitten) {
+  document.getElementById("kittens").classList.remove(Kitten.mood)
+  if (Kitten.affection >= 8) { Kitten.mood = "Happy" }
+  if (Kitten.affection <= 5) { Kitten.mood = "Tolerant" }
+  if (Kitten.affection <= 3) { Kitten.mood = "Angry" }
+  if (Kitten.affection <= 0) { Kitten.mood = "Gone" }
+
+  document.getElementById("kittens").classList.add(Kitten.mood)
+  saveKittens()
 }
 
 /**
  * Removes all of the kittens from the array
  * remember to save this change
  */
-function clearKittens(){
+function clearKittens(kittenId) {
+  let removeKitten = kittens
+  kittens = kittens.filter(kittenId => kittenId !== kittenId)
+  saveKittens()
 }
 
 /**
- * Removes the welcome content and should probably draw the 
+ * Removes the welcome content and should probably draw the
  * list of kittens to the page. Good Luck
  */
 function getStarted() {
   document.getElementById("welcome").remove();
   console.log('Good Luck, Take it away')
+  document.getElementById("submit-name").classList.remove("hidden")
+  drawKittens()
 }
 
 
